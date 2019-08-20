f3675be9e9fc844808c9b4e3311d7e8e91c7909b
Dibora D <diborad@yahoo.com>
2019-08-20 15:06:34 -0500

    all four crystals gernerate a random between 1-12 and assinged the lucky number to a random number 19-120

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,56 @@
-let randomNumber;
-// let a  = crystalOne;
-// let b = crystalTwo;
-// let c = crystalThree;
-// let d = crystalFour;
 let losses = 0;
 let wins = 0;
 let score = 0;
 
+let crystalOne = 0;
+let crystalTwo = 0;
+let crystalThree = 0;
+let crystalFour = 0;
+let myNumber = 0;
+
 window.onload = function() {
-  //   setup();
   setup();
+  crystalOne = getCrystalOne();
+  crystalTwo = getCrystalTwo();
+  crystalThree = getCrystalThree();
+  crystalFour = getCrystalFour();
+
+  myNumber = 0;
 };
 function setup() {
   $("#luckyNumber").text(getLuckyNumber());
   $(".crystalOne").click(function() {
-      $("#myNumber").text(getCrystalOne());
+    myNumber += crystalOne;
+    $("#myNumber").text(myNumber);
   });
-  
+  $(".crystalTwo").click(function() {
+    myNumber += crystalTwo;
+    $("#myNumber").text(myNumber);
+  });
+  $(".crystalThree").click(function() {
+    myNumber += crystalThree;
+    $("#myNumber").text(myNumber);
+  });
+  $(".crystalFour").click(function() {
+    myNumber += crystalFour;
+    $("#myNumber").text(myNumber);
+  });
 }
-function getRandomNumberBeetween(min, max){
-    return Math.floor(Math.random() * Math.floor(max - min) + min);
+function getRandomNumberBeetween(min, max) {
+  return Math.floor(Math.random() * Math.floor(max - min) + min);
 }
 function getLuckyNumber() {
   return getRandomNumberBeetween(19, 120);
 }
-function getCrystalOne(){
-    return getRandomNumberBeetween(1, 12);
+function getCrystalOne() {
+  return getRandomNumberBeetween(1, 12);
 }
-
-
-   
- 
+function getCrystalTwo() {
+  return getRandomNumberBeetween(1, 12);
+}
+function getCrystalThree() {
+  return getRandomNumberBeetween(1, 12);
+}
+function getCrystalFour() {
+  return getRandomNumberBeetween(1, 12);
+}
